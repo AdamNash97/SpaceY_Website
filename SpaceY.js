83049a9a56ff1679b72b12bd4e91d9e5c0eaa96f
Adam Nash <adamnash3112@gmail.com>
2021-02-19 12:17:00 +0000

    Adding MapQuest Co-authored-by: zmervey18@gmail.com <zmervey18@gmail.com>

--- SpaceY.js
+++ SpaceY.js
@@ -11,3 +11,19 @@ function showPage() {
   document.getElementById("myDiv").style.display = "block";
 }
 
+// MapQuest fucionality
+
+window.onload = function() {
+  L.mapquest.key = 'QTckBPKxhqVDwMiXwmIoXC1A4PZcAJ8h';
+
+  var map = L.mapquest.map('map', {
+    center: [40.7128, -74.0059],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 13
+  });
+
+  L.mapquest.directions().route({
+    start: '350 5th Ave, New York, NY 10118',
+    end: 'One Liberty Plaza, New York, NY 10006'
+  });
+}
