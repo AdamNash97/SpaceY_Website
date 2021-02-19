7b6c4a7bd9039321720ccd49b24de9912233462a
Adam Nash <adamnash3112@gmail.com>
2021-02-19 04:34:55 +0000

    Fixed Dropdwon, Implemented Loading Page, Implemented Glowing Header, Button in Photo, Shaking Images.

--- SpaceY.js
+++ SpaceY.js
@@ -1,27 +1,13 @@
-// Placeholder Comment
+// Loader page functionality
 
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
+var myVar;
+
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-    }
-  }
-
-  // Live Stream blink
-
-function blink() {
-    var blinkText = document.getElementById("blink-text");
-    setInterval(function(){
-        blinkText.style.visibility = (blinkText.style.visibility == 'hidden' ? 'visible' : 'hidden'); // Use visibility instead of display so it always takes up space
-    }, 500); // 0.5 seconds
-
+  myVar = setTimeout(showPage, 3000);
 }
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
